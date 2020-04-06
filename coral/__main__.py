a3e10c96edb1cb363976ec0eee34e336b07a8bba
AshleyZG <gsarazh@gmail.com>
2020-04-06 21:01:33 +0800

    refactorize

--- coral/__main__.py
+++ coral/__main__.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 # created by Ge Zhang, Jan 29, 2020
 #
-# test for markdown
-# delete
+# train CORAL
 
 import argparse
 
@@ -10,14 +9,14 @@ from torch.utils.data import DataLoader
 
 from model import BERT
 from trainer import CORALTrainer
-from dataset import DataReader, my_collate, UnitedVocab, SNAPDataset
-import pdb
+from dataset import DataReader, my_collate, UnitedVocab, CORALDataset
+
 import os
 import json
 import torch
 
 
-def train_back_up():
+def train():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-c", "--dataset", required=True,
@@ -30,8 +29,7 @@ def train_back_up():
 
     parser.add_argument("-hs", "--hidden", type=int,
                         default=256, help="hidden size of transformer model")
-    parser.add_argument("-me", "--markdown_emb_size", type=int,
-                        default=256, help="hidden size of transformer model")
+
     parser.add_argument("-l", "--layers", type=int,
                         default=8, help="number of layers")
     parser.add_argument("-a", "--attn_heads", type=int,
@@ -46,7 +44,7 @@ def train_back_up():
     parser.add_argument("-w", "--num_workers", type=int,
                         default=5, help="dataloader worker size")
     parser.add_argument("--duplicate", type=int,
-                        default=5, help="dataloader worker size")
+                        default=5, help="number of negative examples")
     parser.add_argument("--model_path",
                         type=str, help="ex)output/bert.model")
     parser.add_argument("--hinge_loss_start_point", type=int, default=20)
@@ -55,8 +53,7 @@ def train_back_up():
                         help="training with CUDA: true, or false")
     parser.add_argument("--log_freq", type=int, default=10,
                         help="printing loss every n iter: setting n")
-    parser.add_argument("--corpus_lines", type=int,
-                        default=None, help="total number of lines in corpus")
+
     parser.add_argument("--cuda_devices", type=str,
                         default='0', help="CUDA device ids")
     parser.add_argument("--max_graph_num", type=int, default=3000000,
@@ -64,28 +61,14 @@ def train_back_up():
     parser.add_argument("--vocab_path",
                         type=str, help="vocab path")
 
-    parser.add_argument("--on_memory", type=bool, default=True,
-                        help="Loading on memory: true or false")
     parser.add_argument("--n_topics", type=int, default=50)
     parser.add_argument("--lr", type=float, default=1e-3,
                         help="learning rate of adam")
-    parser.add_argument("--adam_weight_decay", type=float,
-                        default=0.01, help="weight_decay of adam")
-    parser.add_argument("--adam_beta1", type=float,
-                        default=0.9, help="adam first beta value")
-    parser.add_argument("--adam_beta2", type=float,
-                        default=0.999, help="adam first beta value")
     parser.add_argument("--dropout", type=float,
                         default=0.2, help="dropout value")
-    parser.add_argument("--weak_supervise", action="store_true")
-    parser.add_argument("--neighbor", action="store_true",
-                        help="force topic distribution over neighbor nodes to be close")
     parser.add_argument("--min_occur", type=int,
                         default=3, help="minimum of occurrence")
-
     parser.add_argument("--use_sub_token", action="store_true")
-    parser.add_argument("--context", action="store_true",
-                        help="use information from neighbor cells")
     parser.add_argument("--markdown", action="store_true", help="use markdown")
     args = parser.parse_args()
 
@@ -93,43 +76,32 @@ def train_back_up():
     output_folder = args.output_path.split('.')[0]
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
-    print("Load Data", args.dataset)
-    data_reader = DataReader(
-        args.dataset, use_sub_token=args.use_sub_token, max_graph_num=args.max_graph_num)
 
+    print("Read Train Data from", args.dataset)
+    data_reader = DataReader(
+        args.dataset, max_graph_num=args.max_graph_num)
+    print("Read Test Data from", args.test_path)
     labeled_data_reader = DataReader(
-        args.test_path, use_sub_token=args.use_sub_token)
-    # pdb.set_trace()
+        args.test_path)
+
     print("Loading Vocab")
-    # if args.markdown:
     vocab = UnitedVocab(data_reader.graphs, min_occur=args.min_occur,
                         use_sub_token=args.use_sub_token, path=args.vocab_path)
-
-    # else:
-    #     vocab = SNAPVocab(data_reader.graphs, min_occur=args.min_occur,
-    #                       use_sub_token=args.use_sub_token)
-    # 有问题，vocab每次不一样。。
-    # pdb.set_trace()
     print("Vocab Size: ", len(vocab))
 
     print("Loading Train Dataset", args.dataset)
-    train_dataset = SNAPDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, seq_len=args.seq_len,
-                                on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics, markdown=args.markdown)
-
+    train_dataset = CORALDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, seq_len=args.seq_len,
+                                 n_neg=args.duplicate, markdown=args.markdown)
     print(len(train_dataset))
-
-    # print("Loading Test Dataset", args.test_dataset)
     print("Loading Dev Dataset", args.dataset)
-    test_dataset = SNAPDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, seq_len=args.seq_len,
-                               on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics, markdown=args.markdown)  # \
+    test_dataset = CORALDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, seq_len=args.seq_len,
+                                n_neg=args.duplicate, markdown=args.markdown)  # \
     print(len(test_dataset))
+    print("Loading Test Dataset", args.dataset)
+    labeled_dataset = CORALDataset(labeled_data_reader.graphs, vocab, seq_len=args.seq_len,
+                                   markdown=args.markdown)
+    print(len(labeled_dataset))
 
-    labeled_dataset = SNAPDataset(labeled_data_reader.graphs, vocab, seq_len=args.seq_len,
-                                  on_memory=args.on_memory, use_sub_token=args.use_sub_token, markdown=args.markdown)
-    # 这里是固定的，prepare_data里seed 被固定了，检查一下vocab
-    # pdb.set_trace()
-    # if args.test_dataset is not None else None
-    # pdb.set_trace()
     print("Creating Dataloader")
     train_data_loader = DataLoader(
         train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
@@ -140,26 +112,22 @@ def train_back_up():
     labeled_data_loader = DataLoader(
         labeled_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
 
-# labeled_data_loader 的 bert input 每次不一样，应该是vocab的问题
-
-    # if test_dataset is not None else None
-    # assert False
-    print("Building BERT model")
+    print("Building CORAL model")
     bert = BERT(len(vocab), hidden=args.hidden,
                 n_layers=args.layers, attn_heads=args.attn_heads, dropout=args.dropout)
 
-    print("Creating BERT Trainer")
+    print("Creating CORAL Trainer")
 
-    trainer = CORALTrainer(bert, len(vocab), train_dataloader=train_data_loader, test_dataloader=test_data_loader,
-                           lr=args.lr, betas=(
-        args.adam_beta1, args.adam_beta2), weight_decay=args.adam_weight_decay,
-        with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, pad_index=vocab.pad_index, model_path=args.model_path, weak_supervise=args.weak_supervise, context=args.context, markdown=args.markdown, hinge_loss_start_point=args.hinge_loss_start_point, entropy_start_point=args.entropy_start_point)
-    # raise NotImplementedError
-    print("Training Start")
+    trainer = CORALTrainer(bert, train_dataloader=train_data_loader, test_dataloader=test_data_loader,
+                           lr=args.lr,
+                           with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, model_path=args.model_path, n_topics=args.n_topics, hinge_loss_start_point=args.hinge_loss_start_point, entropy_start_point=args.entropy_start_point)
 
+    print("Parameters are in {}".format(
+        os.path.join(output_folder, './setting.json')))
     with open(os.path.join(output_folder, './setting.json'), 'w') as fout:
         json.dump(args.__dict__, fout, ensure_ascii=False, indent=2)
-    # pdb.set_trace()
+
+    print("Training Start")
     best_loss = None
     tolerance = 3
     for epoch in range(args.epochs):
@@ -189,4 +157,4 @@ def train_back_up():
     print('Best loss:', best_loss)
 
 
-train_back_up()
+train()
--- coral/test.py
+++ coral/test.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # created by Ge Zhang, Jan 29, 2020
 #
-# test model performance
+# test CORAL performance
 
 import argparse
 
@@ -9,11 +9,10 @@ from torch.utils.data import DataLoader
 
 from model import BERT
 from trainer import CORALTrainer
-from dataset import DataReader, my_collate, UnitedVocab, SNAPDataset
-import pdb
+from dataset import DataReader, my_collate, UnitedVocab, CORALDataset
+
 import os
 import json
-
 import torch
 
 
@@ -22,15 +21,11 @@ def test():
 
     parser.add_argument("-c", "--dataset", required=True,
                         type=str, help="dataset")
-
-    parser.add_argument("-o", "--output_path",
+    parser.add_argument("-o", "--output_path", required=True,
                         type=str, help="ex)output/bert.model")
     parser.add_argument("-t", "--test_path", required=True,
                         type=str, help="ex)output/bert.model")
-
     parser.add_argument("-hs", "--hidden", type=int,
-                        default=256, help="hidden size of transformer model")
-    parser.add_argument("-me", "--markdown_emb_size", type=int,
                         default=256, help="hidden size of transformer model")
     parser.add_argument("-l", "--layers", type=int,
                         default=8, help="number of layers")
@@ -38,57 +33,37 @@ def test():
                         default=8, help="number of attention heads")
     parser.add_argument("-s", "--seq_len", type=int,
                         default=64, help="maximum sequence len")
-
     parser.add_argument("-b", "--batch_size", type=int,
                         default=64, help="number of batch_size")
-    parser.add_argument("-e", "--epochs", type=int,
-                        default=10, help="number of epochs")
     parser.add_argument("-w", "--num_workers", type=int,
                         default=5, help="dataloader worker size")
     parser.add_argument("--duplicate", type=int,
-                        default=5, help="dataloader worker size")
+                        default=5, help="number of negative examples")
     parser.add_argument("--model_path",
                         type=str, help="ex)output/bert.model")
-    parser.add_argument("--vocab_path",
-                        type=str, help="vocab path")
-
     parser.add_argument("--hinge_loss_start_point", type=int, default=20)
     parser.add_argument("--entropy_start_point", type=int, default=30)
     parser.add_argument("--with_cuda", type=bool, default=True,
                         help="training with CUDA: true, or false")
     parser.add_argument("--log_freq", type=int, default=10,
                         help="printing loss every n iter: setting n")
-    parser.add_argument("--corpus_lines", type=int,
-                        default=None, help="total number of lines in corpus")
     parser.add_argument("--cuda_devices", type=str,
                         default='0', help="CUDA device ids")
     parser.add_argument("--max_graph_num", type=int, default=3000000,
                         help="printing loss every n iter: setting n")
-
-    parser.add_argument("--on_memory", type=bool, default=True,
-                        help="Loading on memory: true or false")
+    parser.add_argument("--vocab_path",
+                        type=str, help="vocab path")
     parser.add_argument("--n_topics", type=int, default=50)
     parser.add_argument("--lr", type=float, default=1e-3,
                         help="learning rate of adam")
-    parser.add_argument("--adam_weight_decay", type=float,
-                        default=0.01, help="weight_decay of adam")
-    parser.add_argument("--adam_beta1", type=float,
-                        default=0.9, help="adam first beta value")
-    parser.add_argument("--adam_beta2", type=float,
-                        default=0.999, help="adam first beta value")
     parser.add_argument("--dropout", type=float,
                         default=0.2, help="dropout value")
-    parser.add_argument("--weak_supervise", action="store_true")
-    parser.add_argument("--neighbor", action="store_true",
-                        help="force topic distribution over neighbor nodes to be close")
     parser.add_argument("--min_occur", type=int,
                         default=3, help="minimum of occurrence")
-
     parser.add_argument("--use_sub_token", action="store_true")
-    parser.add_argument("--context", action="store_true",
-                        help="use information from neighbor cells")
     parser.add_argument("--markdown", action="store_true", help="use markdown")
     args = parser.parse_args()
+
     output_folder = args.output_path.split('.')[0]
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
@@ -96,39 +71,30 @@ def test():
 
     print("Load Data", args.dataset)
     data_reader = DataReader(
-        args.dataset, use_sub_token=args.use_sub_token, max_graph_num=args.max_graph_num)
+        args.dataset, max_graph_num=args.max_graph_num)
 
     labeled_data_reader = DataReader(
-        args.test_path, use_sub_token=args.use_sub_token)
+        args.test_path)
 
     print("Loading Vocab")
-    # if args.markdown:
     vocab = UnitedVocab(data_reader.graphs, min_occur=args.min_occur,
-                            use_sub_token=args.use_sub_token, path=args.vocab_path)
-
-    # else:
-    #     vocab = SNAPVocab(data_reader.graphs, min_occur=args.min_occur,
-                          # use_sub_token=args.use_sub_token)
-
+                        use_sub_token=args.use_sub_token, path=args.vocab_path)
     print("Vocab Size: ", len(vocab))
 
     print("Loading Train Dataset", args.dataset)
-    train_dataset = SNAPDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, seq_len=args.seq_len,
-                                on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics, markdown=args.markdown)
+    train_dataset = CORALDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, seq_len=args.seq_len,
+                                 n_neg=args.duplicate, markdown=args.markdown)
 
     print(len(train_dataset))
-
-    # print("Loading Test Dataset", args.test_dataset)
     print("Loading Dev Dataset", args.dataset)
-    test_dataset = SNAPDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, seq_len=args.seq_len,
-                               on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics, markdown=args.markdown)  # \
+    test_dataset = CORALDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, seq_len=args.seq_len,
+                                n_neg=args.duplicate, markdown=args.markdown)  # \
     print(len(test_dataset))
+    print("Loading Test Dataset", args.dataset)
+    labeled_dataset = CORALDataset(labeled_data_reader.graphs, vocab, seq_len=args.seq_len,
+                                   markdown=args.markdown)
+    print(len(labeled_dataset))
 
-    labeled_dataset = SNAPDataset(labeled_data_reader.graphs, vocab, seq_len=args.seq_len,
-                                  on_memory=args.on_memory, use_sub_token=args.use_sub_token, markdown=args.markdown)
-
-    # if args.test_dataset is not None else None
-    # pdb.set_trace()
     print("Creating Dataloader")
     train_data_loader = DataLoader(
         train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
@@ -139,20 +105,17 @@ def test():
     labeled_data_loader = DataLoader(
         labeled_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
 
-    # if test_dataset is not None else None
-    # assert False
-    print("Building BERT model")
+    print("Building CORAL model")
     bert = BERT(len(vocab), hidden=args.hidden,
                 n_layers=args.layers, attn_heads=args.attn_heads, dropout=args.dropout)
 
     print("Creating BERT Trainer")
-    # pdb.set_trace()
-    trainer = CORALTrainer(bert, len(vocab), train_dataloader=train_data_loader, test_dataloader=test_data_loader,
-                           lr=args.lr, betas=(
-        args.adam_beta1, args.adam_beta2), weight_decay=args.adam_weight_decay,
-        with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, pad_index=vocab.pad_index, model_path=args.model_path, weak_supervise=args.weak_supervise, context=args.context, markdown=args.markdown, hinge_loss_start_point=args.hinge_loss_start_point, entropy_start_point=args.entropy_start_point)
-    # raise NotImplementedError
-    print("Training Start")
+
+    trainer = CORALTrainer(bert, train_dataloader=train_data_loader, test_dataloader=test_data_loader,
+                           lr=args.lr,
+                           with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, model_path=args.model_path, n_topics=args.n_topics, hinge_loss_start_point=args.hinge_loss_start_point, entropy_start_point=args.entropy_start_point)
+
+    print("Evaluation Start")
 
     stages, stage_vecs = trainer.api(labeled_data_loader)
 
@@ -169,7 +132,8 @@ def test():
     accuracy = correct / (len(stages) - zero_cells)
     print(zero_cells, 'cells are [PAD]')
     print('Accuracy:', accuracy)
-
+    print('Results are in {}'.format(
+        os.path.join(output_folder, 'test_graphs.txt')))
     with open(os.path.join(output_folder, 'test_graphs.txt'), 'w') as fout:
         for i, g in enumerate(labeled_dataset.graphs):
             g["pred"] = stages[i]
